a2f00486554863358897a9e88cbdd6c66791a0db
Jacob <jerling1989@gmail.com>
2017-11-30 09:28:26 -0500

    mobile nav bug fix

--- assets/js/init.js
+++ assets/js/init.js
@@ -6,8 +6,12 @@ $(document).ready(function(){
   $('.scrollspy').scrollSpy({
   	scrollOffset: 70
   });
+  
   // SIDE NAVIGATION
   $(".button-collapse").sideNav();
+  // Initialize collapsible (uncomment the line below if you use the dropdown variation)
+  $('.collapsible').collapsible();
+
   // PROJECT SLIDER
   $('.slider').slider();
   // STICKY NAVIGATION
