4a6cb6990383b562a80910db8c82b00220e73faf
Jacob <jerling1989@gmail.com>
2017-11-30 10:36:33 -0500

    mobile nav jquery settings

--- assets/js/init.js
+++ assets/js/init.js
@@ -2,22 +2,28 @@ $(document).ready(function(){
 
 	// PARALLAX HEADER
   $('.parallax').parallax();
+
   // SCROLLSPY
   $('.scrollspy').scrollSpy({
   	scrollOffset: 70
   });
-  
-  // SIDE NAVIGATION
-  $(".button-collapse").sideNav();
-  // Initialize collapsible (uncomment the line below if you use the dropdown variation)
-  $('.collapsible').collapsible();
+
+  // MOBILE NAVIGATION
+  $(".button-collapse").sideNav({
+    menuWidth: 150,
+    edge: 'left',
+    closeOnClick: true,
+    draggable: true
+  });
 
   // PROJECT SLIDER
   $('.slider').slider();
+
   // STICKY NAVIGATION
   $('#main-nav').pushpin({
     top: $('#main-nav').offset().top 
   });
+
   // PICTURE CAROUSEL
   $('.carousel').carousel({
   	dist: -25
